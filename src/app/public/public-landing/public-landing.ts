67b33a550f98c520e9a50f5345ce68e43642cf40
Dalton Wood <dwoo642@wgu.edu>
2025-10-23 18:17:33 -0500

    add landing page content

--- src/app/public/public-landing/public-landing.ts
+++ src/app/public/public-landing/public-landing.ts
@@ -1,11 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-public-landing',
-  imports: [],
+  imports: [NgOptimizedImage, MatCardModule, MatButtonModule, RouterLink],
   templateUrl: './public-landing.html',
-  styleUrl: './public-landing.css'
+  styleUrl: './public-landing.css',
 })
-export class PublicLanding {
-
-}
+export class PublicLanding {}
