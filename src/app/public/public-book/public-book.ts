dc51df7ce1114824a4fa1302715a0e2a7a5e1151
Dalton Wood <dwoo642@wgu.edu>
2025-10-25 21:42:10 -0500

    add content and styling to landing page

--- src/app/public/public-book/public-book.ts
+++ src/app/public/public-book/public-book.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-public-book',
-  imports: [],
+  imports: [MatStepperModule],
   templateUrl: './public-book.html',
-  styleUrl: './public-book.css'
+  styleUrl: './public-book.css',
 })
-export class PublicBook {
-
-}
+export class PublicBook {}
--- src/app/public/public-landing/public-landing.ts
+++ src/app/public/public-landing/public-landing.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-public-landing',
-  imports: [NgOptimizedImage, MatCardModule, MatButtonModule, RouterLink],
+  imports: [NgOptimizedImage, MatCardModule, MatButtonModule, RouterLink, MatDividerModule],
   templateUrl: './public-landing.html',
   styleUrl: './public-landing.css',
 })
