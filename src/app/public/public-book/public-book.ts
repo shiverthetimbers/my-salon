81a68f0bab14fb8670a60b9e18ced316676ad766
Dalton Wood <dwoo642@wgu.edu>
2025-10-26 01:09:16 -0500

    add booking date picker and 90-day selectable date range

--- src/app/public/public-book/public-book.ts
+++ src/app/public/public-book/public-book.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatStepperModule } from '@angular/material/stepper';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-public-book',
-  imports: [MatStepperModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatStepperModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './public-book.html',
   styleUrl: './public-book.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PublicBook {}
+export class PublicBook {
+  private _formBuilder = inject(FormBuilder);
+  private readonly _futureDate = new Date();
+  private readonly _currentDay = this._futureDate.getDate();
+
+  readonly minDate = new Date(this._futureDate);
+  readonly maxDate = new Date(this._futureDate.setDate(90 + this._currentDay));
+
+  test() {
+    console.log(this.minDate);
+    console.log(this.maxDate);
+  }
+
+  firstFormGroup = this._formBuilder.group({
+    date: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+}
